c14a91fb2fbeea8b563747f35017bf840ae7b75f
Soledade56 <soledade56@yahoo.com.br>
2025-10-06 11:54:13 -0300

    criação: classe restaurante_dao.dart classe tela_home.dart

---
+++ lib/banco/restaurante_dao.dart
@@ -0,0 +1,18 @@
+import 'package:soledade/banco/database_helper.dart';
+import 'package:soledade/restaurante.dart';
+import 'package:soledade/tipo.dart';
+
+class RestauranteDAO{
+
+  static  Future<List<Restaurante>> listarTodos()async{
+    final db = await DatabaseHelper.getDatabase();
+    final resultado = await db.query('tb_restaurante');
+
+    return resultado.map((mapa){
+      return Restaurante(
+      codigo : mapa['cd_restaurante'] as int,
+      nome: mapa['nm_restaurante'] as String
+      );
+  }).toList();
+  }
+}
